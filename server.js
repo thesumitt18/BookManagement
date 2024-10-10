f45e0d73b429f18daf475335a611c08cc6ac8a6c
thesumitt18 <sumitpatwal.18@gmail.com>
2024-10-10 09:53:46 +0530

    Modified comments

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT;
 
-// Middleware
+// Middlewares
 app.use(cors()); 
 app.use(express.json()); 
 app.use('/uploads', express.static('uploads'));
